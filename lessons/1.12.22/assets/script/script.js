bdf1b677c1f02f3f84dbf570e69fd52ed20150c9
Imanovaayan <imanovaayan214@gmail.com>
2023-01-05 18:58:49 +0400

    update logic

---
+++ lessons/1.12.22/assets/script/script.js
@@ -0,0 +1,38 @@
+// switch (5){
+//     case 2:
+//         alert('2')
+//         break;
+//     case 5:
+//         alert('5')
+//         break;
+// }
+
+// function showMessage(message){
+//     alert(message);
+// };
+// showMessage('baxlama');
+
+
+
+// function question() {
+//   let task = confirm("Do you live in Azerbaijan?"); // yes or not
+//   if (task) {
+//     let age = prompt("How old are you?");
+//   final(age)
+// }
+// }
+// question();
+// function final(vozrast) {
+//   if (18 <= vozrast && vozrast < 65) {
+//     alert("good");
+//   }
+//   else{
+//     alert("sorry")
+//   }
+// }
+
+
+function showMessage(name="Ayan"){
+    return name
+}
+alert(showMessage())
